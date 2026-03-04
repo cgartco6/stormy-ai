b74b0ccde2516b5b4969aeed1092d6f838fee6b9
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2026-03-04 20:24:01 +0200

    Update run_web.bat

--- run_web.bat
+++ run_web.bat
@@ -1,4 +1,10 @@
 @echo off
+REM Change to the directory of this batch file
+cd /d "%~dp0"
+
+REM Activate virtual environment
 call venv\Scripts\activate
+
+REM Run the Flask app
 python -m stormy.api.routes
 pause
